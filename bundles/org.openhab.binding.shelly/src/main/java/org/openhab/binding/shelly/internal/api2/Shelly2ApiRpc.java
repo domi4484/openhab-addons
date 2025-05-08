e07f4d28859ba3857183a92557c76bf9b85a834d
Damiano Lombardi <damiano@opengis.ch>
2025-05-08 21:13:28 +0200

    Fix as per in pr

--- bundles/org.openhab.binding.shelly/src/main/java/org/openhab/binding/shelly/internal/api2/Shelly2ApiRpc.java
+++ bundles/org.openhab.binding.shelly/src/main/java/org/openhab/binding/shelly/internal/api2/Shelly2ApiRpc.java
@@ -771,7 +771,7 @@ public class Shelly2ApiRpc extends Shelly2ApiClient implements ShellyApiInterfac
         logger.error("{}: WebSocket error: {}", thingName, cause.getMessage());
         ShellyThingInterface thing = this.thing;
         if (thing != null && thing.getProfile().alwaysOn) {
-            thingOffline("WebSocket error: %s", cause.getMessage());
+            thingOffline(String.format("WebSocket error: %s", cause.getMessage()));
         }
     }
 
